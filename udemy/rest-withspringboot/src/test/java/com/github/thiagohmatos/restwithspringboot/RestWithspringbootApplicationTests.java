9c4e4e84ed7962112a52cd7a6a788f733fc235d7
thiago <thiagohmatos@gmail.com>
2024-05-11 19:45:45 -0300

    feat(HelloWorld): Starter program

---
+++ udemy/rest-withspringboot/src/test/java/com/github/thiagohmatos/restwithspringboot/RestWithspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.thiagohmatos.restwithspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
